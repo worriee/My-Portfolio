4bf44e8792345fbf031cef9ff532b63219c0de06
worriee <julrymahilum12@gmail.com>
2026-05-16 11:31:03 +0800

    updated resume

--- src/App.jsx
+++ src/App.jsx
@@ -182,7 +182,7 @@ export default function App() {
               About Me
               <img src={arrow} alt="Arrow" className="w-5 h-5 ml-2" />
             </button>
-            <a href="/Mahilum-Resume.docx" download className="flex items-center px-4 py-2 bg-transparent text-black border border-black rounded-md hover:bg-gray-300 hover:text-black transition duration-300 [-webkit-text-stroke:1px_black]">
+            <a href="/My_Resume.pdf" download="Mahilum_Resume.pdf" className="flex items-center px-4 py-2 bg-transparent text-black border border-black rounded-md hover:bg-gray-300 hover:text-black transition duration-300 [-webkit-text-stroke:1px_black]">
               <img src={document} alt="Document" className="w-5 h-5 mr-2" />
               Download Resume
             </a>
